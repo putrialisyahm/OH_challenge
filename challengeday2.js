1c1a4e7627dc13146ef8b61737a44d3cd5de59d2
Putri Alisyah <73177821+putrialisyahm@users.noreply.github.com>
2020-11-18 18:18:39 +0800

    Add files via upload

---
+++ challengeday2.js
@@ -0,0 +1,12 @@
+// Write if-else statements to fulfill the following conditions:
+// if score morethan 60 you pass otherwise you failed
+
+function testScore(num) {
+    if (num > 60) {
+         console.log ('Passed');
+    } else {
+        console.log ('Failed');
+    }
+}
+testScore(50);
+testScore(90);
